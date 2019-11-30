dbbd7ac0cd30b7c3249b6fcdbb3cc8eefc7c0f41
magdazelena <magda.zelezik@gmail.com>
2019-11-30 22:52:12 +0100

    Styles for front page

--- frontned/src/components/Main.js
+++ frontned/src/components/Main.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faBus, faTrain, faCarSide, faBusAlt, faTaxi, faTicketAlt, faLeaf } from '@fortawesome/free-solid-svg-icons'
 
 class Main extends Component {
     constructor(props){
@@ -24,30 +26,59 @@ class Main extends Component {
         
     }
     renderRedirect = ()=>{
-    if(this.state.redirect)
+    if(this.state.redirect){
+        this.setState({
+            redirect: false
+        })
         return <Redirect to={{
             pathname: '/tickets',
-            state: { id: '123' }
+            state: this.state
              }}
         />
+    }
+        
     }
     render(){
         return <div>
             <div className="balanceHeader">
                 <div className='moneyBalance'>
                     <div className="moneyQuota">{this.state.balance} PLN</div>
-                    <div className="moneyAdd" onClick={this.addMoney}>Doładuj</div>
+                    <div className="moneyAdd" onClick={this.addMoney}>
+                        <FontAwesomeIcon icon={faPlus} />
+                        Doładuj
+                    </div>
                 </div>
                 <div className="tokenBalance">
+                    <FontAwesomeIcon icon={faLeaf}/>
                     {this.state.tokens}
+                    <span className="undertitle">Balans tokenów</span>
                 </div>
             </div>
             <div className="options">
                 <div className="item" onClick={this.buyTickets} >
                 {this.renderRedirect()}
+                <FontAwesomeIcon icon={faBus} />
+                    Komunikacja miejska
                 </div>
                 <div className="item">
-
+                    <FontAwesomeIcon icon={faCarSide} />
+                    MobiParking
+                </div>
+                <div className="item">
+                    <FontAwesomeIcon icon={faTrain} />
+                    PKP
+                </div>
+                <div className="item" >
+                <FontAwesomeIcon icon={faBusAlt} />
+                    PKS
+                </div>
+                <div className="item">
+                    <FontAwesomeIcon icon={faTaxi} />
+                    Taxi
+                </div>
+                <div className="item">
+                    <FontAwesomeIcon icon={faTicketAlt} />
+                    Kino
                 </div>
             </div>
         </div>
--- frontned/src/components/Tickets.js
+++ frontned/src/components/Tickets.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-
-const Tickets = () =>{
-    return <div></div>
+class Tickets extends Component{
+    constructor(props){
+        super(props);
+        
+    }
+    render(){
+        console.log(this.state)
+        return <div>
+            <div className="listTickets">
+                <div className="ticket">
+                    Bilet 20 minutowy 
+                </div>
+            </div>
+        </div>
+    }
 }
 
 export default Tickets;
